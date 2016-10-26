9ae6a97c0b9361caed27c19e4a53c838ae4ff726
bdvstg <bdvstg@itri.org.tw>
2016-10-26 08:30:14 +0800

    use libxml to operate xml file of vatic pascal

---
+++ xml_vatic_pascal.h
@@ -0,0 +1,26 @@
+#ifndef XML_VATIC_PASCAL_H__
+#define XML_VATIC_PASCAL_H__
+
+#include <string>
+#include <vector>
+
+typedef struct vatic_object_
+{
+	std::string name;
+	int xmax;
+	int xmin;
+	int ymax;
+	int ymin;
+	int difficult;
+	int occluded;
+	std::string pose;
+	int truncated;
+} vatic_object;
+
+std::vector<vatic_object> xml_vatic_pascal_parse(std::wstring filename);
+
+void xml_vatic_pascal_modifyObjects(std::wstring filename,
+	const std::vector<vatic_object> &objs);
+
+#endif
+
