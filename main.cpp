354b5aa77e2427c745f6d2ce5817de93b15196ca
bdvstg <bdvstg@itri.org.tw>
2016-10-26 11:29:07 +0800

    arrange code and add interactive to mouse
    move onMouse to top
    rename i to curFrame
    rename num to frameNum
    modfiy behavior of select bndbox to cycling
    show which side of selected bndbox is choose by mouse

--- main.cpp
+++ main.cpp
@@ -6,13 +6,44 @@
 #include <thread>
 #include <chrono>
 #include "cv_key_table.h"
+#include "ui_utils.h"
 
 const std::wstring path_xml = L"\\Annotations\\";
 const std::wstring path_img = L"\\JPEGImages\\";
 std::wstring folder;
 std::vector<vatic_object> objs;
 int curObj = 0;
+RECT_DIR curObjSide = RECT_NONE;
 cv::Mat img;
+
+void onMouse(int event = -65535, int x = -65535, int y = -65535, int flags = 0, void* param = 0)
+{//CV_EVENT_LBUTTONDOWN
+    cv::Mat draw = img.clone();
+    cv::circle(draw, cv::Point(x, y), 3, cv::Scalar(255, 0, 0), -1);
+    if (curObj >= 0 && curObj < objs.size())
+    {// curObj is valid
+        const vatic_object & obj = objs[curObj];
+        const cv::Point tl(obj.xmin, obj.ymin);
+        const cv::Point br(obj.xmax, obj.ymax);
+        const cv::Rect bndbox = cv::Rect(tl, br);
+        cv::rectangle(draw, bndbox, cv::Scalar(200, 0, 200), 3);
+
+        cv::Point mousePos = cv::Point(x, y);
+        if (mouseRoi(bndbox).contains(mousePos))
+        {// mouse in ROI of bndbox, should be handle
+            const RECT_DIR dir = rectDir(bndbox, mousePos);
+            Lint_t line = rectLine(bndbox, dir);
+            if (dir == RECT_CENTER)
+                cv::circle(draw, line.p1, 5, cv::Scalar(255, 255, 0), -1);
+            else
+                cv::line(draw, line.p1, line.p2, cv::Scalar(255, 255, 0), 3);
+            curObjSide = dir;
+        }
+        else
+            curObjSide = RECT_NONE;
+    }
+    cv::imshow("img", draw);
+}
 
 void draw_vaticObjs(cv::Mat &img, std::vector<vatic_object> objs)
 {
@@ -46,28 +77,13 @@ void update(cv::Mat &img, std::vector<vatic_object> &objs, const std::wstring & 
 	draw_vaticObjs(img, objs);
 }
 
-void onMouse(int event, int x, int y, int flags, void* param)
-{
-	cv::Mat draw = img.clone();
-	cv::circle(draw, cv::Point(x, y), 3, cv::Scalar(255, 0, 0), -1);
-	if (curObj >= 0 && curObj < objs.size())
-	{
-		const vatic_object & obj = objs[curObj];
-		const cv::Point tl(obj.xmin, obj.ymin);
-		const cv::Point br(obj.xmax, obj.ymax);
-		const cv::Rect bndbox = cv::Rect(tl, br);
-		cv::rectangle(draw, bndbox, cv::Scalar(200, 0, 200), 2);
-	}
-	cv::imshow("img", draw);
-}
-
 int main(int argc, char **argv)
 {
 	folder = OpenFolderDialog();
 	auto xmlfiles = listFiles((folder + path_xml).c_str());
 
-	int num = xmlfiles.size();
-	int i = 0;
+	int frameNum = xmlfiles.size();
+	int curFrame = 0;
 
 	cv::Mat dummy(cv::Size(30, 30), CV_8UC3);
 	cv::imshow("img", dummy);
@@ -87,41 +103,41 @@ int main(int argc, char **argv)
 		}
 		else if (key == CV_KEY_LEFT)
 		{
-			i--;
-			if (i < 0) i = 0;
-			update(img, objs, xmlfiles[i]);
+            curFrame--;
+			if (curFrame < 0) curFrame = 0;
+			update(img, objs, xmlfiles[curFrame]);
 			if (curObj < 0) curObj = 0;
 			if (curObj >= objs.size()) curObj = objs.size() - 1;
 			cv::imshow("img", img); cv::waitKey(1);
 		}
 		else if (key == CV_KEY_RIGHT)
 		{
-			i++;
-			if (i >= num) i = num - 1;
-			update(img, objs, xmlfiles[i]);
-			if (curObj < 0) curObj = 0;
-			if (curObj >= objs.size()) curObj = objs.size() - 1;
+            curFrame++;
+			if (curFrame >= frameNum) curFrame = frameNum - 1;
+			update(img, objs, xmlfiles[curFrame]);
+			if (curObj < 0) curObj = objs.size() - 1;
+			if (curObj >= objs.size()) curObj = 0;
 			cv::imshow("img", img); cv::waitKey(1);
 		}
 		else if (key == CV_KEY_w)
 		{
 			xml_vatic_pascal_modifyObjects(
-				folder + path_xml + xmlfiles[i],
+				folder + path_xml + xmlfiles[curFrame],
 				objs);
 		}
 		else if (key == CV_KEY_UP)
 		{
 			curObj++;
-			if (curObj < 0) curObj = 0;
-			if (curObj >= objs.size()) curObj = objs.size() - 1;
-			onMouse(0, 0, 0, 0, 0);
+			if (curObj < 0) curObj = objs.size() - 1;
+			if (curObj >= objs.size()) curObj = 0;
+			onMouse();
 		}
 		else if (key == CV_KEY_DOWN)
 		{
 			curObj--;
-			if (curObj < 0) curObj = 0;
-			if (curObj >= objs.size()) curObj = objs.size() - 1;
-			onMouse(0, 0, 0, 0, 0);
+			if (curObj < 0) curObj = objs.size() - 1;
+			if (curObj >= objs.size()) curObj = 0;
+			onMouse();
 		}
 		else
 		{
