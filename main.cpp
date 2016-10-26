b94de887bf0c6529240ca3bde980b141d9dba098
bdvstg <bdvstg@itri.org.tw>
2016-10-26 15:25:43 +0800

    collect draw relative code in one side
    draw ui in difference side will cause problem, collect it in one side
    add comment
    rename/arrange global variable

--- main.cpp
+++ main.cpp
@@ -11,11 +11,17 @@
 
 const std::wstring path_xml = L"\\Annotations\\";
 const std::wstring path_img = L"\\JPEGImages\\";
-std::wstring folder;
-std::vector<vatic_object> gObjs;
-int gCurObj = 0;
+std::wstring folder; // root folder (parent of Annotations and JPEGImages)
+std::vector<vatic_object> gObjs; // current images's all bndBoxs
+int gCurObj = 0; // which bndBox in gObjs
 RECT_DIR gCurObjSide = RECT_NONE;
-cv::Mat gImg;
+cv::Mat gImg; // current image
+std::vector<std::wstring> gAllFilenames; // hold all files's filename
+int gFrameNum; // amount of total files, will equal gAllFilenames.size()
+int gCurFrame; // current is nth file
+
+void draw_vaticObjs(cv::Mat &img, std::vector<vatic_object> objs);
+void update();
 
 cv::Rect toRect(const vatic_object &obj)
 {
@@ -32,32 +38,88 @@ void onMouse(int event = -65535, int x = -65535, int y = -65535, int flags = 0, 
     if (event == CV_EVENT_LBUTTONUP)
         mouseLeftBtnDown = false;
 
-    cv::Mat draw = gImg.clone();
-    cv::circle(draw, cv::Point(x, y), 3, cv::Scalar(255, 0, 0), -1);
+    cv::Rect bndbox;
+    bool drawCurObj = false;
+
+    RECT_DIR dir;
+    Lint_t line;
+    bool drawCurObjSide = false;
+
     if (gCurObj >= 0 && gCurObj < gObjs.size())
     {// curObj is valid
-        // draw current bndBox
-        cv::Rect bndbox = toRect(gObjs[gCurObj]);
-        cv::rectangle(draw, bndbox, cv::Scalar(200, 0, 200), 3);
+        drawCurObj = true;
+        vatic_object &targetObj = gObjs[gCurObj];
+        bndbox = toRect(targetObj);
 
         cv::Point mousePos = cv::Point(x, y);
         if (mouseRoi(bndbox).contains(mousePos))
         {// mouse in ROI of bndbox, should be handle
-            const RECT_DIR dir = rectDir(bndbox, mousePos);
+            drawCurObjSide = true;
 
-            // draw current bndBox's selected side
-            Lint_t line = rectLine(bndbox, dir);
-            if (dir == RECT_CENTER)
-                cv::circle(draw, line.p1, 5, cv::Scalar(255, 255, 0), -1);
-            else
-                cv::line(draw, line.p1, line.p2, cv::Scalar(255, 255, 0), 3);
+            dir = rectDir(bndbox, mousePos);
+
+            if (mouseLeftBtnDown &&
+                (event == CV_EVENT_LBUTTONDOWN
+                    || event == CV_EVENT_LBUTTONUP
+                    || event == CV_EVENT_MOUSEMOVE))
+            {
+                if (dir == RECT_UP)
+                {
+                    targetObj.ymin = y;
+                }
+                else if (dir == RECT_DOWN)
+                {
+                    targetObj.ymax = y;
+                }
+                else if (dir == RECT_LEFT)
+                {
+                    targetObj.xmin = x;
+                }
+                else if (dir == RECT_RIGHT)
+                {
+                    targetObj.xmax = x;
+                }
+                else if (dir == RECT_CENTER)
+                {
+
+                }
+                bndbox = toRect(targetObj);
+            }
+            line = rectLine(bndbox, dir);
+
             // update 
             gCurObjSide = dir;
         }
         else
             gCurObjSide = RECT_NONE;
     }
-    cv::imshow("gImg", draw);
+
+    cv::Mat draw = gImg.clone();
+
+    // draw all bndBox
+    draw_vaticObjs(draw, gObjs);
+
+    if (drawCurObj)
+    {// draw current bndBox
+        cv::rectangle(draw, bndbox, cv::Scalar(200, 0, 200), 3);
+
+        if (drawCurObjSide)
+        {// draw current bndBox's selected side
+            if (dir == RECT_CENTER)
+                cv::circle(draw, line.p1, 5, cv::Scalar(255, 255, 0), -1);
+            else
+                cv::line(draw, line.p1, line.p2, cv::Scalar(255, 255, 0), 3);
+        }
+    }
+
+    // draw filename
+    cv::putText(draw, w2mb(gAllFilenames[gCurFrame].c_str()), cv::Point(5, 20), 0, 0.8, cv::Scalar(30, 200, 30), 2);
+
+    // draw mouse pointer
+    cv::circle(draw, cv::Point(x, y), 3, cv::Scalar(255, 0, 0), -1);
+
+    // show
+    cv::imshow("img", draw);
 }
 
 void draw_vaticObjs(cv::Mat &img, std::vector<vatic_object> objs)
@@ -77,34 +139,36 @@ void draw_vaticObjs(cv::Mat &img, std::vector<vatic_object> objs)
     }
 }
 
-void update(cv::Mat &img, std::vector<vatic_object> &objs, const std::wstring & xmlfilename)
+void update()
 {
-    std::wstring xml_name = folder + path_xml + xmlfilename;
-    std::wstring imgfile = xmlfilename;
+    const std::wstring xml_name = folder + path_xml + gAllFilenames[gCurFrame];
+    std::wstring imgfile = gAllFilenames[gCurFrame];
     imgfile.replace(imgfile.length() - 3, 3, L"jpg");
-    std::wstring img_name = folder + path_img + imgfile;
+    const std::wstring img_name = folder + path_img + imgfile;
 
-    objs = xml_vatic_pascal_parse(xml_name);
-    std::vector<char> imgBuf = ReadFile(img_name.c_str());
-    img = cv::imdecode(imgBuf, 1);
-    
-    cv::putText(img, w2mb(xmlfilename.c_str()), cv::Point(5, 20), 0, 0.8, cv::Scalar(30, 200, 30), 2);
-    draw_vaticObjs(img, objs);
+    // update objects
+    gObjs = xml_vatic_pascal_parse(xml_name);
+
+    // update image
+    static std::vector<char> imgBuf = ReadFile(img_name.c_str());
+    gImg = cv::imdecode(imgBuf, 1);
+
+    onMouse();
 }
 
 int main(int argc, char **argv)
 {
     folder = OpenFolderDialog();
-    auto xmlfiles = listFiles((folder + path_xml).c_str());
+    gAllFilenames = listFiles((folder + path_xml).c_str());
 
-    int frameNum = xmlfiles.size();
-    int curFrame = 0;
+    gFrameNum = gAllFilenames.size();
+    gCurFrame = 0;
 
     cv::Mat dummy(cv::Size(30, 30), CV_8UC3);
     cv::imshow("img", dummy);
     cv::setMouseCallback("img", onMouse);
 
-    update(gImg, gObjs, xmlfiles[0]);
+    update();
     cv::imshow("img", gImg); cv::waitKey(1);
     while(true)
     {
@@ -118,18 +182,18 @@ int main(int argc, char **argv)
         }
         else if (key == CV_KEY_LEFT)
         {
-            curFrame--;
-            if (curFrame < 0) curFrame = 0;
-            update(gImg, gObjs, xmlfiles[curFrame]);
+            gCurFrame--;
+            if (gCurFrame < 0) gCurFrame = 0;
+            update();
             if (gCurObj < 0) gCurObj = 0;
             if (gCurObj >= gObjs.size()) gCurObj = gObjs.size() - 1;
             cv::imshow("img", gImg); cv::waitKey(1);
         }
         else if (key == CV_KEY_RIGHT)
         {
-            curFrame++;
-            if (curFrame >= frameNum) curFrame = frameNum - 1;
-            update(gImg, gObjs, xmlfiles[curFrame]);
+            gCurFrame++;
+            if (gCurFrame >= gFrameNum) gCurFrame = gFrameNum - 1;
+            update();
             if (gCurObj < 0) gCurObj = gObjs.size() - 1;
             if (gCurObj >= gObjs.size()) gCurObj = 0;
             cv::imshow("img", gImg); cv::waitKey(1);
@@ -137,7 +201,7 @@ int main(int argc, char **argv)
         else if (key == CV_KEY_w)
         {
             xml_vatic_pascal_modifyObjects(
-                folder + path_xml + xmlfiles[curFrame],
+                folder + path_xml + gAllFilenames[gCurFrame],
                 gObjs);
         }
         else if (key == CV_KEY_UP)
