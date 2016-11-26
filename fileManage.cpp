c047b00e00b9889544b78caed43a25f7297d411b
bdvstg <bdvstg@gmail.com>
2016-11-26 15:21:08 +0800

    fileManage manage filenames by order, is deleted

---
+++ fileManage.cpp
@@ -0,0 +1,65 @@
+#include "fileManage.h"
+
+#include "FileDialogWin.h"
+
+// is filenames(without extname) inside foldXml and foldJpg is all same
+static bool isConsistent(filenames_t xmls, filenames_t jpgs)
+{
+    if (xmls.size() != jpgs.size())
+        return false;
+
+    for (int i = 0; i < (int)xmls.size(); i++)
+    {
+        auto & xmlname = xmls[i];
+        auto & jpgname = jpgs[i];
+        // delete ext name
+        xmlname.erase(xmlname.length() - 4, 4);
+        jpgname.erase(xmlname.length() - 4, 4);
+        // compare
+        if (xmlname.compare(jpgname) != 0)
+            return false; // not equal
+    }
+
+    return true;
+}
+
+static fileManage::frames_t toFrames(
+    const filenames_t & xmls,
+    const filenames_t & deletedXmls)
+{
+    fileManage::frames_t frames;
+    for each (auto name in xmls)
+    {
+        name.erase(name.length() - 4, 4);
+        fileManage::frame_t frame = { name, false };
+        frames.push_back(frame);
+    }
+
+    for each (auto name in deletedXmls)
+    {
+        name.erase(name.length() - 4, 4);
+        fileManage::frame_t frame = { name, true };
+        frames.push_back(frame);
+    }
+}
+
+fileManage::fileManage(foldSetting f)
+    : fold(f)
+{
+        
+}
+
+void fileManage::init()
+{
+    auto xmls = listFiles(fold.fullAnnotations);
+    auto jpgs = listFiles(fold.fullJpegImages);
+    auto deletedXmls = listFiles(fold.fullDeleteAnnotations);
+    auto deletedJpgs = listFiles(fold.fullDeleteJpegImages);
+
+    if (!isConsistent(xmls, jpgs))
+        throw "filenames inside annotations and jpgimages is not same";
+    if (!isConsistent(deletedXmls, deletedJpgs))
+        throw "filenames inside deleted annotations and jpgimages is not same";
+
+    frames = toFrames(xmls, deletedXmls);
+}
---
+++ fileManage.h
@@ -0,0 +1,28 @@
+#ifndef fileManage_H__
+#define fileManage_H__
+
+#include <string>
+#include <vector>
+
+#include "FileDialogWin.h"
+#include "foldSetting.h"
+
+class fileManage
+{
+public:
+    using frame_t =
+    struct {
+        std::wstring baseName; // filename without extname
+        bool isDeleted; // which folder belong, deleted or not
+    };
+    using frames_t = std::vector<frame_t>;
+
+    fileManage(foldSetting f);
+    void init();
+
+private:
+    foldSetting fold;
+    frames_t frames; // more compact struct to hold all filenames
+};
+
+#endif
