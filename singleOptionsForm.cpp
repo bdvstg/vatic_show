70dc36c3d97993ad97bc7d61941d4741b22f9254
bdvstg <bdvstg@gmail.com>
2016-11-29 20:27:00 +0800

    singleOptionsForm is a form that listing radiobox
    singleOptionsForm use emptyForm as it ui
    and add radoi button into it by code (not qt designer)
    the main goal is let select bndBox and labeling classes

---
+++ singleOptionsForm.cpp
@@ -0,0 +1,64 @@
+#include "singleOptionsForm.h"
+
+singleOptionsForm::singleOptionsForm(QWidget *parent, Qt::WindowFlags flags)
+{
+    ui.setupUi(this);
+
+    mScrollArea.reset(new QScrollArea());
+    mScrollArea->setHidden(false);
+    mScrollArea->setWidget(this);
+    mScrollArea->setWidgetResizable(true);
+    this->layout()->setSpacing(1);
+}
+
+void singleOptionsForm::clearOptions()
+{
+    if (mOptions.size() != 0)
+    {
+        for (int i = 0; i < mOptions.size(); i++)
+        {
+            this->layout()->removeWidget(mOptions[i]);
+            mOptions[i]->~QRadioButton();
+            delete mOptions[i];
+            mOptions[i] = nullptr;
+        }
+        mOptions.clear();
+    }
+}
+
+void singleOptionsForm::setOptions(const std::vector<std::string> options)
+{
+    if (mOptions.size() != 0)
+    {
+        clearOptions();
+    }
+
+    mOptions.resize(options.size());
+    for (int i = 0; i < mOptions.size(); i++)
+    {
+        mOptions[i] = new QRadioButton();
+        mOptions[i]->setStyleSheet("::indicator { width: 30px; height: 30px; }");
+        mOptions[i]->setText(options[i].c_str());
+        this->layout()->addWidget(mOptions[i]);
+    }
+}
+
+void singleOptionsForm::setSelected(int i)
+{
+    if (i >= 0 && i < mOptions.size())
+        mOptions[i]->setChecked(true);
+}
+
+int singleOptionsForm::getSelected() const
+{
+    int selected = -1; // no any is checked
+    for (int i = 0; i < mOptions.size(); i++)
+    {
+        if (mOptions[i]->isChecked())
+        {
+            return selected = i;
+            break;
+        }
+    }
+    return selected;
+}
---
+++ singleOptionsForm.h
@@ -0,0 +1,34 @@
+#ifndef singleOptionsForm_H__
+#define singleOptionsForm_H__
+
+#include <memory>
+#include <vector>
+
+#include <QRadioButton>
+#include <QScopedPointer>
+#include <QScrollArea>
+//#include <QLayout>
+#include <QWidget>
+#include "ui_emptyForm.h"
+
+class singleOptionsForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    singleOptionsForm(QWidget *parent = 0, Qt::WindowFlags flags = 0);
+    ~singleOptionsForm() = default;
+
+    void setOptions(const std::vector<std::string> options);
+    void setSelected(int i);
+    int getSelected() const;
+
+private:
+    Ui::emptyForm ui;
+    std::vector<QRadioButton*> mOptions;
+    QScopedPointer<QScrollArea> mScrollArea;
+
+    void clearOptions();
+};
+
+#endif
